1a36742d5657f1a8fa43fb578c89cf12d27cc26f
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-01-08 22:24:38 -0800

    add flip when bouncing off side walls

--- amogus.js
+++ amogus.js
@@ -1,23 +1,29 @@
 class Amogus {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./assets/amogus.png"),
-            0, 250, 180, 250, 12, 0.05);
+
         this.x = 0;
         this.y = 0;
+        this.width = 180;
+        this.height = 250;
         this.xSpeed = 150;
         this.ySpeed = 75;
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./assets/amogus.png"),
+            0, 250, this.width, this.height, 12, 0.05);
     };
     
     update() {
         this.x += this.xSpeed * this.game.clockTick;
         this.y += this.ySpeed * this.game.clockTick;
-        if (this.x > 1024 - 90 || this.x < 0) {
+        if (this.x > 1024 - this.width / 2) {
             this.xSpeed *= -1;
+            this.animator.yStart += 250;
         }
-        if (this.y > 768 - 125 || this.y < 0) {
-            this.ySpeed *= -1;
+        if (this.x < 0) {
+            this.xSpeed *= -1;
+            this.animator.yStart -= 250;
         }
+        if (this.y > 768 - this.height / 2 || this.y < 0) this.ySpeed *= -1;
     };
     
     draw(ctx) {
