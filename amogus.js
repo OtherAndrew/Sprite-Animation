451079a78d001f208a37b82b6d91fca29908c9cb
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-01-08 21:21:23 -0800

    add movement

--- amogus.js
+++ amogus.js
@@ -3,13 +3,17 @@ class Amogus {
         this.game = game;
         this.animator = new Animator(ASSET_MANAGER.getAsset("./assets/amogus.png"),
             0, 250, 180, 250, 12, 0.05);
+        this.x = 0;
+        this.y = 0;
+        this.speed = 150;
     };
     
     update() {
-        
+        this.x += this.speed * this.game.clockTick;
+        if (this.x > 1024) this.x = 0;
     };
     
     draw(ctx) {
-        this.animator.drawFrame(this.game.clockTick, ctx, 25, 25, 0.5);
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y, 0.5);
     };
 }
