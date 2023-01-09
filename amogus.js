574f001319000d2308570c4edcaebb00550921b0
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-01-08 23:43:18 -0800

    eliminate shadow jitter

--- amogus.js
+++ amogus.js
@@ -4,12 +4,12 @@ class Amogus {
 
         this.x = 0;
         this.y = 0;
-        this.width = 180;
+        this.width = 200;
         this.height = 250;
         this.xSpeed = 150;
         this.ySpeed = 75;
         this.animator = new Animator(ASSET_MANAGER.getAsset("./assets/amogus.png"),
-            0, 250, this.width, this.height, 12, 0.05);
+            0, this.height, this.width, this.height, 12, 0.05);
     };
     
     update() {
@@ -17,11 +17,11 @@ class Amogus {
         this.y += this.ySpeed * this.game.clockTick;
         if (this.x > 1024 - this.width / 2 && this.xSpeed > 0) {
             this.xSpeed *= -1;
-            this.animator.yStart += 250;
+            this.animator.yStart += this.height;
         }
         if (this.x < 0 && this.xSpeed < 0) {
             this.xSpeed *= -1;
-            this.animator.yStart -= 250;
+            this.animator.yStart -= this.height;
         }
         if (this.y > 768 - this.height / 2 && this.ySpeed > 0) this.ySpeed *= -1;
         if (this.y < 0 && this.ySpeed < 0) this.ySpeed *= -1;
